58a1f795b7a23378499303382e8291af731c4f91
judeadeniji <judeadeniji66@gmail.com>
2024-04-04 04:10:46 +0100

    second commit

--- app/index.ts
+++
@@ -1,8 +0,0 @@
-import { g_ctx } from "../shared";
-import { bootstrapServer } from "../src/server";
-
-const server = bootstrapServer(g_ctx.config)
-
-server.then((server) => {
-  server.start();
-})
